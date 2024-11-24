f1fdc2c32d86eac004afcd2a96add4c0ff30b359
Paulo Roberto <paulorantunes13@hotmail.com>
2024-11-24 15:39:58 -0300

    feat: Creating the Menu component , its style and styles global

---
+++ pomodog/src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { TimerComponent } from '../components/timer/timer.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [TimerComponent, HeaderComponent, FooterComponent],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}

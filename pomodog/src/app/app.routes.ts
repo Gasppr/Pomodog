73be5061e7ece06b28467ce143d6155ab42b5d57
Paulo Roberto <paulorantunes13@hotmail.com>
2024-12-07 17:15:23 -0300

    chore: Update file of routes

--- pomodog/src/app/app.routes.ts
+++ pomodog/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
-  {path: "" ,component: MenuComponent}
+  {path: "pomodog" ,component: MenuComponent}
 ];

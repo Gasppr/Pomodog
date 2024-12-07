af0c648dab4f96c8d0334ad3fab763b55cf729a7
Paulo Roberto <paulorantunes13@hotmail.com>
2024-12-07 16:56:18 -0300

    feat: Criação do display de contagem e sua lógica de contagem de tempo

--- pomodog/src/app/components/timer/timer.component.ts
+++ pomodog/src/app/components/timer/timer.component.ts
@@ -1,12 +1,75 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'timer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './timer.component.html',
-  styleUrl: './timer.component.scss'
+  styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent {
+  countdown: object;
+  private intervalId: any | null = null;
+  timer: number;
+  activateControls = false;
+  isPaused = false;
 
+  countsDog: string[] = [
+    'horário que terminou o 1°',
+    'horário que terminou o 2°',
+    '3',
+    '4',
+    '5',
+    '6',
+  ];
+
+  constructor() {
+    this.timer = 0;
+    this.countdown = {
+      background: `conic-gradient(#497ef0 ${this.timer}%, 0, transparent)`,
+    };
+  }
+
+  startTimer() {
+    console.log('start');
+    this.activateControls = true;
+    this.isPaused = false;
+    this.runTimer();
+  }
+
+  pauseTimer() {
+    console.log('pause');
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+      this.isPaused = true;
+    }
+  }
+
+  stopTimer() {
+    console.log('restart');
+    this.pauseTimer();
+    this.timer = 0;
+    this.isPaused = false;
+    this.activateControls = false;
+    this.updateCountdown();
+  }
+
+  private runTimer() {
+    this.intervalId = setInterval(() => {
+      if (this.timer < 100) {
+        this.timer++;
+        this.updateCountdown();
+      } else {
+        this.stopTimer();
+      }
+    }, 1000 * 18);
+  }
+
+  private updateCountdown() {
+    this.countdown = {
+      background: `conic-gradient(#497ef0 ${this.timer}%, 0, transparent)`,
+    };
+  }
 }
